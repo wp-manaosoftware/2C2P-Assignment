41fe2110768959c5df2072c011038720c9b5cce3
Warm <wp@manaosoftware.com>
2019-07-10 10:29:19 +0700

    Add transection model

---
+++ API/API/Core/DomainModels/Transections/Status.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Core.DomainModels.Transections
+{
+    public enum Status
+    {
+        Success,
+        Failed,
+        Canceled
+    }
+}
---
+++ API/API/Core/DomainModels/Transections/Transection.cs
@@ -0,0 +1,16 @@
+﻿using API.Core.DomainModels.Transections;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Core.DomainModels
+{
+    public class Transection
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string CurrencyCode { get; set; }
+        public Status Status { get; set; }
+    }
+}
