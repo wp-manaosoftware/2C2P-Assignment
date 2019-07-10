8b3b28a9697abeaf2b4e72646e24bc8d8e5939d2
Warm <wp@manaosoftware.com>
2019-07-10 13:53:35 +0700

    Add customer controller

---
+++ API/API/Controllers/CustomerController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using API.Core.DomainModels;
+using API.Infrastructure.EF.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [Route("api/customers")]
+    [ApiController]
+    public class CustomerController : ControllerBase
+    {
+        private ICustomerService customerService { get; }
+        public CustomerController(ICustomerService customerService)
+        {
+            this.customerService = customerService;
+        }
+        
+        [HttpGet("{customerId}")]
+        public async Task<ActionResult<Customer>> GetById(int customerId)
+        {
+            return await customerService.GetById(customerId);
+        }
+    }
+}
