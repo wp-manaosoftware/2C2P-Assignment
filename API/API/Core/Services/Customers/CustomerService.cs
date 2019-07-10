cf9f42643a26e77bc0176e890a8f4d04c29ee707
Warm <wp@manaosoftware.com>
2019-07-10 13:52:28 +0700

    Add customer services

---
+++ API/API/Core/Services/Customers/CustomerService.cs
@@ -0,0 +1,19 @@
+﻿using API.Core.DomainModels;
+using API.Infrastructure.EF.Repositories;
+using System.Threading.Tasks;
+
+namespace API.Infrastructure.EF.Services
+{
+    public class CustomerService : ICustomerService
+    {
+        private ICustomerRepository customerRepository { get; }
+        public CustomerService(ICustomerRepository customerRepository)
+        {
+            this.customerRepository = customerRepository;
+        }
+        public async Task<Customer> GetById(int customerId)
+        {
+            return await customerRepository.GetById(customerId);
+        }
+    }
+}
---
+++ API/API/Core/Services/Customers/ICustomerService.cs
@@ -0,0 +1,13 @@
+﻿using API.Core.DomainModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Infrastructure.EF.Services
+{
+    public interface ICustomerService
+    {
+        Task<Customer> GetById(int Id);
+    }
+}
