e673fba68117f23bfab0f004110b5742385c0615
Warm <wp@manaosoftware.com>
2019-07-10 11:46:44 +0700

    Add customer repository

---
+++ API/API/Infrastructure/EF/Repositories/CustomerRepository.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using API.Core.DomainModels;
+using Microsoft.EntityFrameworkCore;
+using WebAPI.Infrastructure.EF;
+
+namespace API.Infrastructure.EF.Repositories
+{
+    public class CustomerRepository : ICustomerRepository
+    {
+        public AssignmentDbContext context { get; set; }
+
+        public CustomerRepository(AssignmentDbContext context)
+        {
+            this.context = context;
+        }
+
+        public async Task<Customer> GetCustomerById(int customerId)
+        {
+            return await context.Customers.SingleOrDefaultAsync(_ => _.Id == customerId);
+        }
+    }
+}
---
+++ API/API/Infrastructure/EF/Repositories/ICustomerRepository.cs
@@ -0,0 +1,13 @@
+﻿using API.Core.DomainModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Infrastructure.EF.Repositories
+{
+    public interface ICustomerRepository
+    {
+        Task<Customer> GetCustomerById(int Id);
+    }
+}
