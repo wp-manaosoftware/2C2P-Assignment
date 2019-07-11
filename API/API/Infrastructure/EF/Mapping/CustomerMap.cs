df6c0bbeb219f0ea9d6cb0b37c029fb6735084d3
Warm <wp@manaosoftware.com>
2019-07-11 09:26:58 +0700

    Add db constrain

--- API/API/Infrastructure/EF/Mapping/CustomerMap.cs
+++ API/API/Infrastructure/EF/Mapping/CustomerMap.cs
@@ -9,10 +9,10 @@ namespace RoadWatch.Infrastructure.DAL.EF.Mappings
         {
             var entity = modelBuilder.Entity<Customer>();
             entity.ToTable("Customers");
-            entity.Property(c => c.Id).ValueGeneratedOnAdd();
-            entity.Property(c => c.Name);
-            entity.Property(c => c.ContactEmail);
-            entity.Property(c => c.MobileNo);
+            entity.Property(c => c.Id).ValueGeneratedOnAdd().HasMaxLength(999999999);
+            entity.Property(c => c.Name).HasMaxLength(30);
+            entity.Property(c => c.ContactEmail).HasMaxLength(25);
+            entity.Property(c => c.MobileNo).HasMaxLength(10);
             entity.Property(c => c.CreatedDate);
             entity.Property(c => c.UpdatedDate);
             entity.HasMany(c => c.Transactions).WithOne(c => c.Customer).HasForeignKey(c => c.CustomerId);
