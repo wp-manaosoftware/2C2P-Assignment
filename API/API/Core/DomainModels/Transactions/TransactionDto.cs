32f093f4777c34ee5010e213e9b9831e07677d22
Warm <wp@manaosoftware.com>
2019-07-11 09:26:03 +0700

    Update display data shape

--- API/API/Core/DomainModels/Transactions/TransactionDto.cs
+++ API/API/Core/DomainModels/Transactions/TransactionDto.cs
@@ -8,8 +8,18 @@ namespace API.Core.DomainModels
     {
         public int Id { get; set; }
         public string CurrencyCode { get; set; }
-        public Decimal Amount { get; set; }
+        public Decimal Amount
+        {
+            get { return Math.Round(_amount, 2); }
+            set { _amount = value; }
+        }
         public Status Status { get; set; }
+        public string FormattedTransactionDate => GetFormattedTransactionDate();
         public DateTimeOffset TransactionDate { get; set; }
-    }
+        private string GetFormattedTransactionDate()
+        {
+            return TransactionDate.ToString("dd/MM/yyyy HH:MM");
+        }
+        private Decimal _amount { get; set; }
+}
 }
